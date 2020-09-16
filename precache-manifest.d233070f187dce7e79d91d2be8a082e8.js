51b31804d0876308000025ada88ca7bad2be38ce
Atala <atala.rchidi@gmail.com>
2020-09-15 20:25:02 -0400

    Updates

---
+++ precache-manifest.d233070f187dce7e79d91d2be8a082e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce34f4c077c5aeba5a3380ebda4baf7",
+    "url": "/one-note/index.html"
+  },
+  {
+    "revision": "d4b6bb38f922d12328bc",
+    "url": "/one-note/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "089b8c9572f390272a72",
+    "url": "/one-note/static/js/2.a482a509.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/one-note/static/js/2.a482a509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4b6bb38f922d12328bc",
+    "url": "/one-note/static/js/main.23b86b71.chunk.js"
+  },
+  {
+    "revision": "f7e788f9c22af204b72d",
+    "url": "/one-note/static/js/runtime-main.45390fa9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/one-note/static/media/logo.5d5d9eef.svg"
+  }
+]);
