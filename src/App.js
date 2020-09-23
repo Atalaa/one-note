e5bd5550f503002ec97904b76a18f03f82445028
Atala <atala.rchidi@gmail.com>
2020-09-23 16:38:07 -0400

    created notes for testing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import Header from './components/Header';
 import Note from './components/Note';
 import Footer from './components/Footer';
+import notes from './data/notes';
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Note />
+
+      {
+        notes.map(item => {
+          return (
+            <Note key={item.key} title={item.title} content={item.content}/>
+          )
+        })
+      }
+      
       <Footer />
     </div>
   );
