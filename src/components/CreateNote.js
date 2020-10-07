9afe7b0277c97a450ff4c5e2475510b21545a621
Atala <atala.rchidi@gmail.com>
2020-10-07 19:06:15 -0400

    added event functions

--- src/components/CreateNote.js
+++ src/components/CreateNote.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import { useState } from 'react';
 
 const CreateNote = () => {
+  const [note,setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote, [name]: value
+      };
+    });
+  }
+
+  const submitNote = (e) => {
+    e.preventDefault(); 
+  }
+
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input 
+          name="title" 
+          onChange={handleChange} 
+          value={note.title} 
+          placeholder="Title" 
+        />
+
+        <textarea 
+          name="content" 
+          onChange={handleChange} 
+          value={note.content} 
+          placeholder="Take a note..." 
+          rows="3" 
+        />
+
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
